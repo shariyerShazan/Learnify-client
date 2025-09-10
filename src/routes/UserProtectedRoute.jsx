73d2c157d1016ecbe138c8ee4f08e701d4e1878d
shariyerShazan <shariyershazan1@gmail.com>
2025-09-10 13:36:06 +0600

    protected routes

---
+++ src/routes/UserProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+import { toast } from "react-toastify";
+
+const UserProtectedRoute = ({ children }) => {
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      toast.warning("You must login to access this page!");
+    }
+  }, [user]);
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default UserProtectedRoute;
